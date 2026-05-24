eab01d4ec853e8b1afb3ca2cdda1f38047ba9ea3
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2026-05-24 21:30:50 +0200

    feat(LRU): refactor and pin/unpin logic

--- InfuseDB/src/infusedb/FileEngine/lru.rs
+++ InfuseDB/src/infusedb/FileEngine/lru.rs
@@ -13,6 +13,7 @@ struct Node<T> {
     prev: *mut Node<T>,
     data: T,
     key: u32,
+    pin: usize,
 }
 
 impl<T> Node<T> {
@@ -22,6 +23,7 @@ impl<T> Node<T> {
             prev: null_mut(),
             data,
             key,
+            pin: 0,
         }
     }
 }
@@ -38,7 +40,7 @@ impl<T> LRU<T> {
     }
 
     pub fn get(&mut self, id: u32) -> Option<&T> {
-        let ptr = self.map.get(&id)?.clone();
+        let ptr = *self.map.get(&id)?;
         self.move_to_head(ptr);
         unsafe {
             if !ptr.is_null() {
@@ -49,8 +51,84 @@ impl<T> LRU<T> {
         }
     }
 
-    fn evict(&mut self) {
-        todo!()
+    pub fn pin(&mut self, id: u32) -> Result<(), &'static str> {
+        let ptr = *self.map.get(&id).ok_or("Not found")?;
+        unsafe {
+            (*ptr).pin += 1;
+        }
+        Ok(())
+    }
+
+    pub fn unpin(&mut self, id: u32) -> Result<(), &'static str> {
+        let ptr = *self.map.get(&id).ok_or("Not found")?;
+        unsafe {
+            (*ptr).pin -= 1;
+        }
+        Ok(())
+    }
+
+    pub fn write<F, R>(&mut self, id: u32, f: F) -> Option<R>
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        let ptr = *self.map.get(&id)?;
+        self.move_to_head(ptr);
+        unsafe {
+            (*ptr).pin += 1;
+            let result = f(&mut (*ptr).data);
+            (*ptr).pin -= 1;
+            Some(result)
+        }
+    }
+
+    fn evict(&mut self) -> Result<(), &'static str> {
+        if self.tail.is_null() {
+            return Err("Tail is null");
+        }
+        let mut candidate = self.tail;
+        unsafe {
+            while !candidate.is_null() {
+                if (*candidate).pin == 0 {
+                    break;
+                }
+                candidate = (*candidate).prev;
+            }
+            if candidate.is_null() {
+                return Err("no candidate found, all elements pinned");
+            }
+            self.detach(candidate);
+            self.count -= 1;
+            self.map.remove(&(*candidate).key);
+            drop(Box::from_raw(candidate));
+        }
+        Ok(())
+    }
+
+    fn detach(&mut self, node: *mut Node<T>) {
+        unsafe {
+            if (*node).prev.is_null() {
+                // Is head
+                self.head = (*node).next;
+                if !self.head.is_null() {
+                    (*self.head).prev = null_mut();
+                } else {
+                    //only item
+                    self.tail = null_mut();
+                }
+            } else if (*node).next.is_null() {
+                //Is tail
+                self.tail = (*node).prev;
+                if !self.tail.is_null() {
+                    (*self.tail).next = null_mut();
+                } else {
+                    //only item
+                    self.head = null_mut();
+                }
+            } else {
+                (*(*node).prev).next = (*node).next;
+                (*(*node).next).prev = (*node).prev;
+            }
+        }
     }
 
     fn move_to_head(&mut self, node: *mut Node<T>) {
@@ -59,21 +137,8 @@ impl<T> LRU<T> {
                 return;
             }
 
-            if !(*node).prev.is_null() {
-                (*(*node).prev).next = (*node).next
-            }
-
-            if !(*node).next.is_null() {
-                (*(*node).next).prev = (*node).prev;
-            } else {
-                // Node is Tail
-                self.tail = if self.tail.is_null() {
-                    node
-                } else if !(*node).prev.is_null() {
-                    (*node).prev
-                } else {
-                    self.tail
-                }
+            if !(*node).prev.is_null() || !(*node).next.is_null() {
+                self.detach(node);
             }
 
             (*node).prev = null_mut();
@@ -82,12 +147,15 @@ impl<T> LRU<T> {
                 (*self.head).prev = node;
             }
             self.head = node;
+            if self.tail.is_null() {
+                self.tail = self.head
+            }
         }
     }
 
     pub fn set(&mut self, key: u32, data: T) -> Result<(), &'static str> {
         if self.count >= self.max_nodes {
-            self.evict();
+            self.evict()?;
         }
         let node = Node::new(key, data);
         let ptr = Box::into_raw(Box::new(node));
@@ -145,6 +213,88 @@ mod tests {
         unsafe {
             assert_eq!((*cache.head).key, 0);
             assert_eq!((*cache.tail).key, 1);
+        }
+    }
+
+    #[test]
+    fn test_eviction() {
+        let mut cache = LRU::<String>::new(2);
+        let payload = "Hello World".to_string();
+        assert_eq!(cache.count, 0);
+        let r = cache.set(0, payload.clone());
+        let r = cache.set(1, payload.clone());
+        let r = cache.set(2, payload.clone());
+        assert!(r.is_ok());
+        assert_eq!(cache.count, 2);
+        assert!(!cache.head.is_null());
+        assert!(!cache.tail.is_null());
+        unsafe {
+            assert_eq!((*cache.head).key, 2);
+            assert_eq!((*cache.tail).key, 1);
+        }
+    }
+
+    #[test]
+    fn test_eviction_pin() {
+        let mut cache = LRU::<String>::new(2);
+        let payload = "Hello World".to_string();
+        assert_eq!(cache.count, 0);
+        let _ = cache.set(0, payload.clone());
+        let _ = cache.set(1, payload.clone());
+        let r = cache.pin(0);
+        assert!(r.is_ok());
+        let _ = cache.set(2, payload.clone());
+
+        assert_eq!(cache.count, 2);
+        assert!(!cache.head.is_null());
+        assert!(!cache.tail.is_null());
+        unsafe {
+            assert_eq!((*cache.head).key, 2);
+            assert_eq!((*cache.tail).key, 0);
+        }
+    }
+
+    #[test]
+    fn test_eviction_pin_all() {
+        let mut cache = LRU::<String>::new(2);
+        let payload = "Hello World".to_string();
+        assert_eq!(cache.count, 0);
+        let _ = cache.set(0, payload.clone());
+        let _ = cache.set(1, payload.clone());
+        let r = cache.pin(0);
+        assert!(r.is_ok());
+        let r = cache.pin(1);
+        assert!(r.is_ok());
+        let r = cache.set(2, payload.clone());
+        assert!(r.is_err());
+        assert_eq!(cache.count, 2);
+        assert!(!cache.head.is_null());
+        assert!(!cache.tail.is_null());
+        unsafe {
+            assert_eq!((*cache.head).key, 1);
+            assert_eq!((*cache.tail).key, 0);
+        }
+    }
+
+    #[test]
+    fn test_eviction_unpin() {
+        let mut cache = LRU::<String>::new(2);
+        let payload = "Hello World".to_string();
+        assert_eq!(cache.count, 0);
+        let _ = cache.set(0, payload.clone());
+        let _ = cache.set(1, payload.clone());
+        let _ = cache.pin(0);
+        let _ = cache.pin(1);
+        let r = cache.unpin(1);
+        assert!(r.is_ok());
+        let r = cache.set(2, payload.clone());
+        assert!(r.is_ok());
+        assert_eq!(cache.count, 2);
+        assert!(!cache.head.is_null());
+        assert!(!cache.tail.is_null());
+        unsafe {
+            assert_eq!((*cache.head).key, 2);
+            assert_eq!((*cache.tail).key, 0);
         }
     }
 }
